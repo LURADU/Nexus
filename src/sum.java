e0b863bfe9ea9fd1d42647c4917855cdbac60339
LURADU <radulung14@gmail.com>
2024-06-21 20:20:15 +0300

    add sum

---
+++ src/sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+    int sum = 0;
+    int a, b, c;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c = sc.nextInt();
+    sum = a + b + c;
+ }
+
+}
